3c0c33932491825dfa9b2654ce45f7a8653495ac
= <=>
2022-01-04 15:59:57 -0500

    reactiondiffusing

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Reaction-Diffusion-Systems
+based off of Karl Sims' Reaction-Diffusion Tutorial : https://www.karlsims.com/rd.html
---
+++ reaction_diffusion.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter.ttk import *
+
+root = Tk()
+
+root.title("First_Program")
+
+label = Label(root, text ="Hello World !").pack()
+
+root.mainloop()
