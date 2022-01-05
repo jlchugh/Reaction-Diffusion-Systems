bd317a89e77288a5b33c9320584fdb20e1e4d64f
= <=>
2022-01-04 22:38:51 -0500

    finished preliminatry calculation of new concentrations

--- reaction_diffusion.py
+++ reaction_diffusion.py
@@ -23,16 +23,20 @@ bottom_center = .2
 bottom_right = .05
 
 # prompt the user for the height, width, feed_rate, kill_rate, delta_t, and diffusion rates for A and B
+
+
 def get_user_input():
-    feedrate = float(input("enter feed rate: "))
+    feed_rate = float(input("enter feed rate: "))
     kill_rate = float(input("enter kill rate: "))
     height = int(input("enter height: "))
     width = int(input("enter width: "))
     delta_t = float(input("enter delta_t: "))
-    d_a = float(input("enter diffusion rate for A: "))
-    d_b = float(input("enter diffusion rate for B: "))
+    d_A = float(input("enter diffusion rate for A: "))
+    d_B = float(input("enter diffusion rate for B: "))
 
 # prompt the user for a 3x3 kernel to define the laplacian via a 3x3 convolution
+
+
 def get_kernel():
     top_left = float("enter 3x3 convolution matrix - top left: ")
     top_center = float("enter 3x3 convolution matrix - top center: ")
@@ -45,6 +49,8 @@ def get_kernel():
     bottom_right = float("enter 3x3 convolution matrix - bottom right: ")
 
 # create a 2d grid of pairs that represents areas of distinct concentration of chemical A and B (not individual particls)
+
+
 def initialize_grid(height=1000, width=1000):
     grid = []
     for x in range(width):
@@ -56,7 +62,32 @@ def initialize_grid(height=1000, width=1000):
 
 # give a 2x2 grid and return a 2x2 grid with the convolution matrix applied to all elements
 # need to make sure that the edges are not affected; we will keep them the same
-def calculate_new_concentrations(grid, delta_t, current_t):
+def calculate_new_concentrations(grid, time):
+    # for each item in the matrix
     for x in width:
         for y in height:
-            
+            # only calculate values within a frame of thickness 1
+            # johnny you fucked this up you need to figure out how to do both a and b
+            if x >= 1 and x <= width - 1 and y >= 1 and y <= height - 1:
+                nabla_A = top_left * grid[x - 1][y - 1][0] + top_center * grid[x - 1][y][0] + top_right * grid[x - 1][y + 1][0] + middle_left * grid[x][y - 1][0] +
+                middle_center * grid[x][y][0] + middle_right * grid[x][y + 1][0] + \
+                    bottom_left * grid[x + 1][y - 1][0] + \
+                    bottom_center * grid[x + 1][y][0]
+                + bottom_right * grid[x + 1][y + 1][0]
+
+                nabla_B = top_left * grid[x - 1][y - 1][1] + top_center * grid[x - 1][y][1] + top_right * grid[x - 1][y + 1][1] + middle_left * grid[x][y - 1][1] +
+                middle_center * grid[x][y][1] + middle_right * grid[x][y + 1][1] + \
+                    bottom_left * grid[x + 1][y - 1][1] + \
+                    bottom_center * grid[x + 1][y][1]
+                + bottom_right * grid[x + 1][y + 1][1]
+
+                a = grid[x][y][0]
+                b = grid[x][y][1]
+                # A' = A + (d_A*nabla^2*A-AB^2 + f*(1-A))*delta_t
+                new_A = a + (d_A * nabla_A**2 * a - a * b **
+                             2 + feed_rate * (1 - a)) * delta_t
+                # B' = B+(d_B*nabla^2*B+AB^2 - (k+f)B)*delta_t
+                new_B = b + (d_B * nabla_B**2 * b * a * b**2 -
+                             (kill_rate + feed_rate) * b) * delta_t
+                grid[x][y][0] = new_A
+                grid[x][y][1] = new_B
